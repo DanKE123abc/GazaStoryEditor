3edb5bd2f2ada5d68943cf34b0ef87554d78c057
DanKe <1586017466@qq.com>
2023-07-18 00:38:30 +0800

    v1

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Gaza;
+using UnityEngine;
+
+namespace NewBehaviourScript
+{
+
+	///<summary>
+	///脚本名称： NewBehaviourScript.cs
+	///修改时间：
+	///脚本功能：
+	///备注：
+	///</summary>
+
+	public class NewBehaviourScript : MonoBehaviour
+	{
+    	void Start()
+    	{
+	        
+    	}
+
+    	void Update()
+    	{
+	        if (Input.GetKeyDown(KeyCode.Space))
+	        {
+		        GazaStoryManager storyManager = GetComponent<GazaStoryManager>();
+		        storyManager.Play();
+	        }
+    	}
+	}
+
+}
